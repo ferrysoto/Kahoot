4211fd82ba2eff681beb8a6d5cd329cb34d3b165
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-12-02 17:41:12 +0100

    bootstrap configuration

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link rel="stylesheet" href="./public/css/main.css">
+    <link rel="stylesheet" href="./public/css/bootstrap.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="utf-8">
+    <script type="text/javascript" src="./public/js/bootstrap.js"></script>
     <title>Kahoot</title>
   </head>
   <body>

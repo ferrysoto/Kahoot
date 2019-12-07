54136be420f204b7dac50b817cb45c746c9b1a7d
Ferran Herrero Soto <41327308+ferrysoto@users.noreply.github.com>
2019-12-07 16:27:57 +0100

    Update form

--- resources/views/create_account.php
+++ resources/views/create_account.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	
-</head>
-<body background="http://orig11.deviantart.net/20eb/f/2015/030/6/f/_minflat__dark_material_design_wallpaper__4k__by_dakoder-d8fjqzu.jpg">
-<div class="login-page">
-  <div class="form">
-    <form action="confirmationAccountCreator.php" method="POST" class="login-form">
-      <input type="text" name="username" placeholder="username"/>
-      <input type="password" name="password" placeholder="password"></br>
-      <input type="text" name="name" placeholder="name"/>
-      <input type="text" name="email" placeholder="email"/></br>
-      <select name="typeAccount">
-        <option value='1'>Profesor</option>      
-      </select></br>
-      
-
-      <button><span>Sign Up</span></button>
-
-    </form>
-  </div>
-</div>
-</body>
+	<head>	
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="login-page">
+		  <div class="form">
+		    <form action="confirmationAccountCreator.php" method="POST" class="login-form">
+		      <input type="text" name="username" placeholder="username"/>
+		      <input type="password" name="password" placeholder="password"></br>
+		      <input type="text" name="name" placeholder="name"/>
+		      <input type="text" name="email" placeholder="email"/></br>
+		      <select name="typeAccount">
+			<option value='1'>Teacher</option>      
+			<option value='2'>Student</option>      
+			<option value='3'>Socially</option>      
+			<option value='4'>Work</option>      			      
+		      </select></br>
+		      <button><span>Sign Up</span></button>
+		    </form>
+		  </div>
+		</div>
+	</body>
 </html>

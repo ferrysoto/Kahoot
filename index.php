7eb61ffc6ea8a714d5d506370a779b6170595067
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-12-07 16:58:24 +0100

    Home page update

--- index.php
+++ index.php
@@ -4,12 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="./public/css/main.css">
-    <link rel="stylesheet" href="./public/css/bootstrap.css">
-    <script type="text/javascript" src="./public/js/bootstrap.js"></script>
-    <script type="text/javascript" src="./public/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <title>Kahoot</title>
   </head>
   <body>
@@ -29,6 +28,35 @@
         </li>
       </ul>
     </nav>
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-6">
+    			<div class="custom-jumbotron">
+    				<h2>
+    					Hello, world!
+    				</h2>
+    				<p>
+    					This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+    				</p>
+    				<p>
+    					<a class="btn btn-primary btn-large" href="#">Learn more</a>
+    				</p>
+    			</div>
+    		</div>
+    		<div class="col-md-6">
+          <video width="100%" height="100%" autoplay muted controls id="home-video">
+            <source src="./resources/video/home.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+          </video>
+    		</div>
+    	</div>
+    </div>
+
+
+
+
+
+
     <div class="col-md-12">
       <!-- Modal Log in-->
       <div class="modal fade" id="loginUpModal" tabindex="-1" role="dialloginUpModalog" aria-labelledby="loginUpModal" aria-hidden="true">

cefa4958121c4cd27ac68499518c8db911427daa
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-12-09 19:44:55 +0100

    update absolute routes to relative

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="/public/css/main.css">
+    <link rel="stylesheet" href="./public/css/main.css">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -14,7 +14,7 @@
   <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-between">
       <ul class="navbar-nav">
-        <a href="/">
+        <a href="./">
           <img src="./resources/logo.png" alt="Logo Kahoot" width="100px" height="auto" style="filter: brightness(100);">
         </a>
       </ul>
@@ -46,12 +46,12 @@
                           </div>
                       </div>
                       <div id="video-bg">
-                        <img src="/resources/img/phone.png" alt="phone-template">
+                        <img src="./resources/img/phone.png" alt="phone-template">
                       </div>
                       <div id="video-home">
-                        <video width="305" height="700" src="/resources/video/home-mobile.mp4" autobuffer="" autoplay="" muted="" loop="" webkit-playsinline="true" playsinline="true">
+                        <video width="305" height="700" src="./resources/video/home-mobile.mp4" autobuffer="" autoplay="" muted="" loop="" webkit-playsinline="true" playsinline="true">
                           <div class="fallback">
-                            <img src="/resources/img/phone.png" alt="mobile" width="700" height="auto">
+                            <img src="./resources/img/phone.png" alt="mobile" width="700" height="auto">
                           </div>
                         </video>
                       </div>

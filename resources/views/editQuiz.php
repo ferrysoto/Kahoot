4686c6cd56db99e9bc0928acd4a044b9f41e8672
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-12-19 17:10:02 +0100

    update edit quiz

--- resources/views/editQuiz.php
+++ resources/views/editQuiz.php
@@ -7,67 +7,41 @@
               <b>1. Question</b>
               <img src="../img/cover.png" alt="Question cover" width="80%" height="auto">
             </div>
-            <div class="row d-flex justify-content-around" tabindex="0">
-              <div class="btn-group dropright">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Add question
-                </button>
-                <div class="dropdown-menu" style="background-color: #f1f1f1">
-                  <a class="dropdown-item" href="#" style="color: #e11c3c">Quiz</a>
-                  <a class="dropdown-item" href="#" style="color: #1368ce">True or false</a>
-                  <a class="dropdown-item" href="#" style="color: #ffa602">Multiple Choice</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#" style="color: #26890c">Velocity</a>
-                </div>
-              </div>
-            </div>
           </div>
+          <hr>
 
           <div class="row" tabindex="2">
             <div class="row d-flex justify-content-around">
-              <b>1. Question</b>
+              <b>2. Question</b>
               <img src="../img/cover.png" alt="Question cover" width="80%" height="auto">
             </div>
-            <div class="row d-flex justify-content-around" tabindex="0">
-              <div class="btn-group dropright">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Add question
-                </button>
-                <div class="dropdown-menu" style="background-color: #f1f1f1">
-                  <a class="dropdown-item" href="#" style="color: #e11c3c">Quiz</a>
-                  <a class="dropdown-item" href="#" style="color: #1368ce">True or false</a>
-                  <a class="dropdown-item" href="#" style="color: #ffa602">Multiple Choice</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#" style="color: #26890c">Velocity</a>
-                </div>
-              </div>
-            </div>
           </div>
+          <hr>
 
           <div class="row" tabindex="2">
             <div class="row d-flex justify-content-around">
-              <b>1. Question</b>
+              <b>3. Question</b>
               <img src="../img/cover.png" alt="Question cover" width="80%" height="auto">
             </div>
-            <div class="row d-flex justify-content-around" tabindex="0">
-              <div class="btn-group dropright">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Add question
-                </button>
-                <div class="dropdown-menu" style="background-color: #f1f1f1">
-                  <a class="dropdown-item" href="#" style="color: #e11c3c">Quiz</a>
-                  <a class="dropdown-item" href="#" style="color: #1368ce">True or false</a>
-                  <a class="dropdown-item" href="#" style="color: #ffa602">Multiple Choice</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#" style="color: #26890c">Velocity</a>
-                </div>
+          </div>
+          <div class="row d-flex justify-content-around" tabindex="0">
+            <div class="btn-group dropright">
+              <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Add question
+              </button>
+              <div class="dropdown-menu" style="background-color: #f1f1f1">
+                <a class="dropdown-item" href="#" style="color: #e11c3c">Quiz</a>
+                <a class="dropdown-item" href="#" style="color: #1368ce">True or false</a>
+                <a class="dropdown-item" href="#" style="color: #ffa602">Multiple Choice</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" style="color: #26890c">Velocity</a>
               </div>
             </div>
           </div>
 
         </div>
         <div class="col-md-10" style="background-color: #f2f2f2;">
-          <input type="text" name="" value="" placeholder="Click to start typing your question">
+          <input type="text" name="" value="" placeholder="Click to start typing your question" style="width: 90%;">
         </div>
     </div>
 </div>

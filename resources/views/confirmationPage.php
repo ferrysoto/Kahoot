af0ac052cdb59cd5b22357be48badc658be4aaf6
Ferran Herrero <ferran.herrero@EUROCASTELAO.COM>
2020-01-09 12:21:58 +0100

    fix route mail

--- resources/views/confirmationPage.php
+++ resources/views/confirmationPage.php
@@ -2,17 +2,17 @@
 	include '../helps/actionsDatabase.php';
 	// if(isset($_GET['token'])){
 	// }
-	
+
 	$result = getCreatorByToken($_GET['token']);
 	if (empty($result)){
 		echo "Tu cuenta no esta activada porque no has creado ninguna cuenta.";
 	}else{
 		$id = $result[0];
-	
+
 		changeAccessPermissionTrue($id);
-		header('Location: ./dashboardPage.php?token='.$tokenURL.'');
-		
+		header('Location: ../../index.php');
+
 	}
-	
+
 
 ?>
